9ed696713d89a762ccd94e28c00df7a1968b471a
Mark Dologuin <magliter@gmail.com>
2014-11-09 00:07:51 -0500

    Location fixed, queries everytime it moves

--- app/src/main/java/application/areyousafe/MainActivity.java
+++ app/src/main/java/application/areyousafe/MainActivity.java
@@ -14,12 +14,30 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.LatLng;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 
 public class MainActivity extends FragmentActivity implements LocationListener{
@@ -56,17 +74,83 @@ public class MainActivity extends FragmentActivity implements LocationListener{
             }
         }
 
+
+
         //set onClick Listener
         GetMyLocationButton = (Button) findViewById(R.id.MainButton);
 
         //BUTTON FUNCTION
+
         GetMyLocationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                map.animateCamera(CameraUpdateFactory.zoomTo(18));
 
             }
         });
 
+        //Create HTTP
+        createHTTP();
+
+
+    }//End of MAIN
+
+    public void createHTTP(){
+        Log.e("ENTERING", "createHTTP");
+        JSONArray jArray = null;
+        /*
+        String result = null;
+        StringBuilder sb = null;
+        InputStream is = null;
+
+
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+
+        //http post
+        try{
+            HttpClient httpclient = new DefaultHttpClient();
+
+            HttpPost httppost = new HttpPost("http://mgltr.root.sx/query.php");
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            is = entity.getContent();
+            Log.e ("log_tag_test", "hello" + entity.toString());
+        }catch(Exception e){
+            Log.e("log_tag", "Error in http connection"+e.toString());
+        }
+
+       //convert response to string
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
+            sb = new StringBuilder();
+            sb.append(reader.readLine() + "\n");
+
+            String line="0";
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            result=sb.toString();
+        }catch(Exception e){
+            Log.e("log_tag", "Error converting result "+e.toString());
+        }
+
+        String name;
+        try{
+            jArray = new JSONArray(result);
+            JSONObject json_data = null;
+            for(int i=0;i<jArray.length();i++){
+                json_data = jArray.getJSONObject(i);
+                String ct_name = json_data.getString("NAME");//here "Name" is the column name in database
+            }
+        }
+        catch(JSONException e1){
+            Toast.makeText(getBaseContext(), "No Data Found", Toast.LENGTH_LONG).show();
+        } catch (ParseException e1) {
+            e1.printStackTrace();
+        }
+        */
     }
 
 
@@ -96,7 +180,7 @@ public class MainActivity extends FragmentActivity implements LocationListener{
         LatLng lastLng = new LatLng(location.getLatitude(),location.getLongitude());
 
         map.moveCamera(CameraUpdateFactory.newLatLng(lastLng));
-        map.animateCamera(CameraUpdateFactory.zoomTo(14));
+        map.animateCamera(CameraUpdateFactory.zoomTo(16));
 
 
     }
